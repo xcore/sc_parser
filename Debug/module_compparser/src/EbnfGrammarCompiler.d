51da46d8cfa1816fbd59de0619dd14a3e33d9375
Bacchi Raffaele <bacchi_raffaele@mailcity.com>
2011-12-28 10:44:36 +0100

    Added Xml parser sample application

--- Debug/module_compparser/src/EbnfGrammarCompiler.d
+++
@@ -1,9 +0,0 @@
- \
- module_compparser/src/EbnfGrammarCompiler.d module_compparser/src/EbnfGrammarCompiler.o :  \
- ../module_compparser/src/EbnfGrammarCompiler.c \
-  ../module_compparser/src/EbnfGrammarCompiler.h \
-  ../module_compparser/src/CompParser.h
-
-../module_compparser/src/EbnfGrammarCompiler.h:
-
-../module_compparser/src/CompParser.h:
